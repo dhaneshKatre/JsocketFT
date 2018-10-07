80d5d84ed31a908560945f212c1feaf83977f9b4
dhaneshKatre <dhaneshkatre@gmail.com>
2018-10-07 20:41:53 +0530

    aes encryption

--- FileClient.java
+++ FileClient.java
@@ -1,5 +1,13 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.net.*;
+import java.nio.*;
+import java.security.NoSuchAlgorithmException;
+import java.util.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.IllegalBlockSizeException;
+import javax.crypto.spec.*;
 
 public class FileClient implements Runnable {
 
@@ -11,10 +19,11 @@ public class FileClient implements Runnable {
   private static final int PORT = 1337;
   private static final String root = System.getProperty("user.dir");
   private static String lastRequest;
-
+  private static byte[] key = new byte[16];
+  private static RSA rsa;
   public static void main(String[] args) {
-    //System.out.println(root);
     try {
+      rsa = new RSA();
       InetAddress cordAddress = InetAddress.getLocalHost(); //server's address
       socket = new Socket(cordAddress, PORT);
       inputLine = new BufferedReader(new InputStreamReader(System.in));
@@ -36,39 +45,106 @@ public class FileClient implements Runnable {
         os.close();
         is.close();
         socket.close();
-      } catch(IOException e) {
+      } catch(Exception e) {
           e.printStackTrace();
       }
     }
   }
 
-  public void getFile(String fName, Long size) {
+  public void getKey() {
     try {
-      int bytesRead, current = 0;
-      byte[] buffer = new byte[size.intValue()];
-      DataInputStream ips = new DataInputStream(socket.getInputStream());
-      BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(root +"\\"+ fName));
-      while(size > 0 && (bytesRead = ips.read(buffer, 0, buffer.length)) != -1) {
-        bos.write(buffer, 0, bytesRead);
-        size -= bytesRead;
-        bos.flush();
-      }
-      System.out.println("File " + fName + " downloaded successfully!");
-    } catch(Exception r){
-        System.out.println("Download Unsuccessful " + r.getMessage());
+      BigInteger privateKey = rsa.getPrivateKey();
+      BigInteger[] data = new BigInteger[16];
+      for(int i=0;i<data.length;i++)
+        data[i] = new BigInteger(is.readLine());
+      System.out.println("Decoding Key!");
+      for(int i=0;i<data.length;i++)
+        key[i] = (byte) rsa.decode(privateKey, data[i]);
+      System.out.println("Initialization Successful!");
+    } catch(Exception r) {
+      r.printStackTrace();
     }
   }
+
+  public void getAesFile(String fName, Long size) {
+    try {
+      byte[] ctArr = new byte[size.intValue()];
+      DataInputStream dis = new DataInputStream(socket.getInputStream());
+      dis.read(ctArr);
+      dis.close();
+      ByteBuffer buffer = ByteBuffer.wrap(ctArr);
+      int iv = buffer.getInt();
+      if(iv < 12 || iv >= 16)
+        throw new IllegalArgumentException("Wrong ivL");
+      byte[] iv2 = new byte[iv];
+      buffer.get(iv2);
+      byte[] cipherTxt = new byte[buffer.remaining()];
+      buffer.get(cipherTxt);
+      byte[] pt = AES.decode(key, cipherTxt, iv2);
+      BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(root +"/"+ fName));
+      bos.write(pt);
+      System.out.println("Decryption Successful!");
+    } catch(Exception r) {
+      r.printStackTrace();
+    }
+  }
+
+  // public void getEncryptedFile(String fName, Long size) {
+  //   try {
+  //     BigInteger[] data = new BigInteger[size.intValue()];
+  //     byte[] buffer = new byte[size.intValue()];
+  //     for(int i=0;i<data.length;i++)
+  //       data[i] = new BigInteger(is.readLine());
+  //     System.out.println("File " + fName + " downloaded successfully!");
+  //     System.out.println("Decrypting...");
+  //     BigInteger privateKey = rsa.getPrivateKey();
+  //     for(int i=0;i<data.length;i++)
+  //       buffer[i] = (byte)rsa.decode(privateKey, data[i]);
+  //     BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(root +"\\"+ fName));
+  //     bos.write(buffer);
+  //     System.out.println("Decryption Successful!");
+  //   } catch(Exception e) {
+  //     e.printStackTrace();
+  //   }
+  // }
+
+  // public void getFile(String fName, Long size) {
+  //   try {
+  //     int bytesRead, current = 0;
+  //     byte[] buffer = new byte[size.intValue()];
+  //     DataInputStream ips = new DataInputStream(socket.getInputStream());
+  //     BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(root +"\\"+ fName));
+  //     while(size > 0 && (bytesRead = ips.read(buffer, 0, buffer.length)) != -1) {
+  //       bos.write(buffer, 0, bytesRead);
+  //       size -= bytesRead;
+  //       bos.flush();
+  //     }
+  //     System.out.println("File " + fName + " downloaded successfully!");
+  //   } catch(Exception r){
+  //       System.out.println("Download Unsuccessful " + r.getMessage());
+  //   }
+  // }
 
   public void run() {
     String response;
     try {
+      System.out.println(is.readLine());
+      System.out.println(is.readLine());
+      BigInteger n = new BigInteger(is.readLine());
+      BigInteger phi = new BigInteger(is.readLine());
+      BigInteger publicKey = new BigInteger(is.readLine());
+      rsa.setN(n);
+      rsa.setPhi(phi);
+      rsa.setPublicKey(publicKey);
+      rsa.generatePrivate(publicKey);
+      getKey();
       while((response = is.readLine()) != null) {
         System.out.println(response);
         if(response.equalsIgnoreCase("Hope to see you again! :-D Bye."))
           System.exit(0);
-        else if(response.contains(" bytes file incoming...")) {
+        else if(response.contains(" bytes AES encrypted file incoming...")) {
           System.out.println("lastRequest: " + lastRequest);
-          synchronized(this){getFile(lastRequest, Long.parseLong(response.split("[ \t]+")[0]));}
+          synchronized(this){getAesFile(lastRequest, Long.parseLong(response.split("[ \t]+")[0]));}
         }
       }
       closed = true;
@@ -76,4 +152,52 @@ public class FileClient implements Runnable {
         e.printStackTrace();
     }
   }
+}
+
+class AES {
+  public static byte[] decode(byte[] key, byte[] ctb, byte[] iv) {
+    try {
+      SecretKeySpec secretKeySpec = new SecretKeySpec(key, "AES");
+      final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+      cipher.init(Cipher.DECRYPT_MODE, secretKeySpec, new GCMParameterSpec(128, iv));
+      byte[] pt = cipher.doFinal(java.util.Base64.getDecoder().decode(ctb));
+      return pt;
+    } catch(Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+}
+
+class RSA {
+    private BigInteger publicKey, phi, privateKey, n;
+
+    public void generatePrivate(BigInteger publicKey) {
+        this.privateKey = publicKey.modInverse(phi);
+    }
+
+    public BigInteger getPrivateKey() {
+      return this.privateKey;
+    }
+
+    public void setPhi(BigInteger phi) {
+      this.phi = phi;
+    }
+
+    public void setPublicKey(BigInteger publicKey) {
+      this.publicKey = publicKey;
+    }
+
+    public void setN(BigInteger n) {
+      this.n = n;
+    }
+
+    public int decode(BigInteger privateKey, BigInteger ct) {
+        try {
+            return (ct.pow(privateKey.intValue()).mod(n)).intValue();
+        } catch(ArithmeticException e) {
+            e.printStackTrace();
+            return -99;
+        }
+    }
 }
--- FileServer.java
+++ FileServer.java
@@ -1,5 +1,14 @@
 import java.io.*;
+import java.math.BigInteger;
 import java.net.*;
+import java.nio.*;
+import java.security.SecureRandom;
+import java.util.*;
+
+import javax.crypto.Cipher;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.GCMParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
 
 public class FileServer {
   private static ServerSocket ss = null;
@@ -12,51 +21,53 @@ public class FileServer {
     try {
       ss = new ServerSocket(PORT);
     } catch (IOException e) {
-        System.out.println(e);
+      System.out.println(e);
     }
-      while(true) {
-        try {
-          socket = ss.accept();
-          int i = 0;
-          for(i = 0; i < MAX_CLIENTS; i++) {
-            if(threads[i] == null) {
-              (threads[i] = new ClientThread(socket)).start();
-              break;
-            }
+    while (true) {
+      try {
+        socket = ss.accept();
+        int i = 0;
+        for (i = 0; i < MAX_CLIENTS; i++) {
+          if (threads[i] == null) {
+            (threads[i] = new ClientThread(socket)).start();
+            break;
           }
-          if (i == MAX_CLIENTS) {
-            PrintStream os = new PrintStream(socket.getOutputStream());
-            os.println("Server too busy. Try later.");
-            os.close();
-            socket.close();
-          }
-        } catch(IOException r){
-            r.printStackTrace();
         }
+        if (i == MAX_CLIENTS) {
+          PrintStream os = new PrintStream(socket.getOutputStream());
+          os.println("Server too busy. Try later.");
+          os.close();
+          socket.close();
+        }
+      } catch (IOException r) {
+        r.printStackTrace();
+      }
     }
   }
 }
 
 class ClientThread extends Thread {
-
   DataInputStream is = null;
   PrintStream ps = null;
   Socket clientSocket = null;
   BufferedInputStream bis = null;
   OutputStream os = null;
   final File root = new File(System.getProperty("user.dir"));
-  
+  RSA rsa = null;
+  AES aes = null;
   public void writeToClient(String msg) {
     ps.println(msg);
   }
 
   public ClientThread(Socket cs) {
     this.clientSocket = cs;
+    this.rsa = new RSA();
+    this.aes = new AES();
     try {
       is = new DataInputStream(cs.getInputStream());
       ps = new PrintStream(cs.getOutputStream());
-    } catch(IOException e) {
-        e.printStackTrace();
+    } catch (IOException e) {
+      e.printStackTrace();
     }
   }
 
@@ -64,20 +75,20 @@ class ClientThread extends Thread {
     try {
       System.out.println(fName);
       final File folder = new File(fName);
-      if(folder.isDirectory() && folder.exists()) {
-        if(!(folder.list().length > 0)) {
+      if (folder.isDirectory() && folder.exists()) {
+        if (!(folder.list().length > 0)) {
           writeToClient("Directory is empty!");
           return;
         }
       }
-      for(final File file : folder.listFiles()) {
-        if(file.isDirectory())
-          writeToClient("<DIR> "+file.getName());
+      for (final File file : folder.listFiles()) {
+        if (file.isDirectory())
+          writeToClient("<DIR> " + file.getName());
         else
-          writeToClient("<FILE> "+file.getName());
+          writeToClient("<FILE> " + file.getName());
       }
-    } catch(Exception r) {
-        writeToClient("File does not exist.");
+    } catch (Exception r) {
+      writeToClient("File does not exist.");
     }
   }
 
@@ -85,55 +96,175 @@ class ClientThread extends Thread {
     try {
       System.out.println(fName);
       final File fileToSend = new File(fName);
-      writeToClient(fileToSend.length() + " bytes file incoming...");
-      byte[] sendArray = new byte[(int)fileToSend.length()];
+      byte[] sendArray = new byte[(int) fileToSend.length()];
       bis = new BufferedInputStream(new FileInputStream(fileToSend));
       bis.read(sendArray, 0, sendArray.length);
+
+      //*************AES*************//
+      byte[] encryptedFile = aes.encrypt(sendArray);
+      byte[] aesIv = aes.getIV();
+      ByteBuffer byteBuffer = ByteBuffer.allocate(4 + aesIv.length + encryptedFile.length);
+      byteBuffer.putInt(aesIv.length);
+      byteBuffer.put(aesIv);
+      byteBuffer.put(encryptedFile);
+      byte[] bufferToSend = byteBuffer.array();
+      writeToClient(bufferToSend.length + " bytes AES encrypted file incoming...");
+
+      //********Sending fie as byte array********//
       os = clientSocket.getOutputStream();
-      os.write(sendArray, 0, sendArray.length);
+      os.write(bufferToSend, 0, bufferToSend.length);
       os.flush();
-      return true;
-    } catch(Exception e) {
-        e.printStackTrace();
-        return false;
+      return true;      
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
     }
   }
 
   public void run() {
-	System.out.println(root);
+    System.out.println(root);
     writeToClient("***Welcome to the Cloud***");
-    while(true) {
+    writeToClient("Initializing Transfer Parameters..");
+    writeToClient(rsa.getN().toString());
+    writeToClient(rsa.getPhi().toString());
+    writeToClient(rsa.getPublicKey().toString());
+    byte[] secretKey = aes.getKey();
+    BigInteger publicKey = rsa.getPublicKey();
+    BigInteger[] data = new BigInteger[secretKey.length];
+    for (int i = 0; i < secretKey.length; i++) {
+        data[i] = rsa.encode(publicKey, (int) secretKey[i]);
+        writeToClient(data[i].toString());
+    }
+    writeToClient("Parameter Initialization Completed!");
+    while (true) {
       try {
         String request = is.readLine().trim();
-        if(request.startsWith("quit")) {
+        if (request.startsWith("quit")) {
           writeToClient("Hope to see you again! :-D Bye.");
           clientSocket.close();
           System.exit(0);
           break;
-        }
-        else if(request.startsWith("ls")) {
+        } else if (request.startsWith("ls")) {
           try {
-            lS(root.getAbsolutePath() +"/"+ (request.split("[ \t]+"))[1]);
-          } catch(ArrayIndexOutOfBoundsException r) {
-              writeToClient("Path is not specified, showing root directory contents..");
-              lS(root.getAbsolutePath());
+            lS(root.getAbsolutePath() + "/" + (request.split("[ \t]+"))[1]);
+          } catch (ArrayIndexOutOfBoundsException r) {
+            writeToClient("Path is not specified, showing root directory contents..");
+            lS(root.getAbsolutePath());
           }
-        }
-        else if(request.startsWith("get")) {
+        } else if (request.startsWith("get")) {
           try {
-            if(sendFile(root.getAbsolutePath() +"/"+ (request.split("[ \t]+"))[1]))
+            if (sendFile(root.getAbsolutePath() + "/" + (request.split("[ \t]+"))[1]))
               writeToClient("Transfer Successful!");
             else
               writeToClient("Transfer Failed!");
-            } catch(ArrayIndexOutOfBoundsException e) {
-                writeToClient("File is not specified!");
-            }
+          } catch (ArrayIndexOutOfBoundsException e) {
+            writeToClient("File is not specified!");
+          }
         }
-      } catch(IOException e) {
-          e.printStackTrace();
-      } catch(Exception r) {
-          r.printStackTrace();
+      } catch (IOException e) {
+        e.printStackTrace();
+      } catch (Exception r) {
+        r.printStackTrace();
       }
+    }
+  }
+}
+
+class AES {
+  private byte[] key, iv;
+  private SecretKey secretKey;
+  private GCMParameterSpec parameterSpec;
+  private SecureRandom secureRandom = new SecureRandom();
+  public AES() {
+    this.key = new byte[16];
+    this.iv = new byte[12];
+    secureRandom.nextBytes(key);
+    secretKey = new SecretKeySpec(key, "AES");
+    secureRandom.nextBytes(iv);
+    parameterSpec = new GCMParameterSpec(128, this.iv);
+  }
+
+  public byte[] getIV() {
+    return this.iv;
+  }
+
+  public byte[] getKey() {
+    return this.key;
+  }
+
+  public byte[] encrypt(byte[] pt) {
+    try {
+      final Cipher cipher = Cipher.getInstance("AES/GCM/NoPadding");
+      cipher.init(Cipher.ENCRYPT_MODE, this.secretKey, this.parameterSpec);
+      byte[] ctb = cipher.doFinal(pt);
+      ctb = Base64.getEncoder().encode(ctb);
+      return ctb;
+    } catch(Exception e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+}
+
+class RSA {
+  private BigInteger publicKey, privateKey;
+  private BigInteger p, q, n, phi;
+  private final Random rnd = new Random();
+  private static final int bitLength = 10;
+
+  public RSA() {
+    p = BigInteger.probablePrime(bitLength, rnd);
+    q = BigInteger.probablePrime(bitLength, rnd);
+    n = p.multiply(q);
+    phi = p.subtract(BigInteger.ONE).multiply(q.subtract(BigInteger.ONE));
+    generatePublic();
+    // generatePrivate(this.publicKey);
+  }
+
+  private void generatePublic() {
+    BigInteger e = BigInteger.probablePrime(bitLength / 2, rnd);
+    while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(phi) < 0)
+      e.add(BigInteger.ONE);
+    this.publicKey = e;
+  }
+
+  public BigInteger getPublicKey() {
+    return this.publicKey;
+  }
+
+  public BigInteger getPhi() {
+    return this.phi;
+  }
+
+  public BigInteger getN() {
+    return this.n;
+  }
+
+  private void generatePrivate(BigInteger publicKey) {
+    this.privateKey = publicKey.modInverse(phi);
+  }
+
+  public BigInteger getPrivateKey() {
+    return this.privateKey;
+  }
+
+  public BigInteger encode(BigInteger publicKey, int pt) {
+    try {
+      BigInteger m = BigInteger.valueOf(pt);
+      return m.pow(publicKey.intValue()).mod(n);
+    } catch (ArithmeticException e) {
+      e.printStackTrace();
+      return null;
+    }
+  }
+
+  public int decode(BigInteger privateKey, BigInteger ct) {
+    try {
+      return (ct.pow(privateKey.intValue()).mod(n)).intValue();
+    } catch (ArithmeticException e) {
+      e.printStackTrace();
+      return -99;
     }
   }
 }
